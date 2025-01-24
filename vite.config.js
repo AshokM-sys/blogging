dae8164d110fb0b78a519e34a5a42948aa22339a
Ashok M <ashok1363.com@gmail.com>
2025-01-24 08:02:27 +0530

    Fix entry point for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',  // If your source code is inside the src folder
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx', // Set the entry point here
-    }
-  }
+  plugins: [react()],
 });
